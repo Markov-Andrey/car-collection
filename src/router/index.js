ae889e99e7f1470d9c951cab4214187306066e56
Andre Markov <andremarkov@icloud.com>
2025-06-18 14:52:22 +0300

    upd

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import CarsPage from '../views/CarsPage.vue'
 import CarPage from '../views/CarPage.vue'
 
+const base = import.meta.env.BASE_URL
+
 const routes = [
     { path: '/', name: 'CarsPage', component: CarsPage },
     { path: '/:id', name: 'CarPage', component: CarPage, props: true },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(base),
     routes,
 })
 
