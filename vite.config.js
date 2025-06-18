e184fd84988b0076372ba85c6bafdad20fe7d1d7
Andre Markov <andremarkov@icloud.com>
2025-06-18 14:41:59 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import pages from 'vite-plugin-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-      tailwindcss(),
-  ],
+    base: '/car-collection/',
+    plugins: [
+        vue(),
+        tailwindcss(),
+        pages()
+    ]
 })
